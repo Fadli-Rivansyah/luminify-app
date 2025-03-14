eae1c914503507289adb811f87eac8ae915d6469
fadli rivansyah <fadlirvnsyah@gmail.com>
2025-03-14 11:26:16 +0700

    upload to github

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
       </header>
       <div className="flex justify-between my-[5em]">
         <header className="w-1/2">
-          <h1 className="capitalize text-2xl lg:text-5xl text-gray-900 dark:text-gray-100 font-bold">buat efek blur dengan cepat!</h1>
-          <p className="leading-8 text-gray-400 max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores labore magnam exercitationem, aliquam nisi nostrum tempora assumenda rerum, laborum tempore sapiente? Ipsam ut natus soluta dolores ab vero reiciendis aspernatur.
+          <h1 className="capitalize text-2xl lg:text-5xl text-gray-900 dark:text-gray-100 font-bold">Change the Look with Elegant Backdrop Effects!</h1>
+          <p className="leading-8 text-gray-400 max-w-xl">Create modern designs with backdrop blur, opacity, brightness, contrast, hue-rotate, saturate, sepia, and more! Luminify lets you customize the look of your background to perfection, creating an elegant Glassmorphism effect in seconds. Swipe, copy the Tailwind code, and make your design more futuristic! 🚀✨
           </p>
         </header>
         <div className="w-1/2">
--- src/components/BlurTool.jsx
+++ src/components/BlurTool.jsx
@@ -1,13 +1,33 @@
 import React, { useRef, useState } from "react";
 
 const BlurTool = () => {
-
     const [value, setValue] = useState(0);
     const [copy, setCopy] = useState(false);
     const codeRef = useRef(null)
     
-    const handleBlur = (e) => {
-        setValue(e.target.value);
+    const [filterState, setFilterState] = useState({
+        satuan: "px",
+        maxValue: 200,
+        filter: "blur",
+      });
+
+    const filterOptions = {
+        blur: { satuan: "px", maxValue: 1000 },
+        brightness: { satuan: "%", maxValue: 500 },
+        contrast: { satuan: "%", maxValue: 500 },
+        grayscale: { satuan: "%", maxValue: 100 },
+        "hue-rotate": { satuan: "deg", maxValue: 360 },
+        invert: { satuan: "%", maxValue: 100 },
+        opacity: { satuan: "%", maxValue: 100 },
+        saturate: { satuan: "%", maxValue: 500 },
+        sepia: { satuan: "%", maxValue: 100 }
+    };
+
+    const handleOption = (e) => {
+        const item = e.target.value;
+        if (filterOptions[item]) {
+            setFilterState({ ...filterOptions[item], filter: item });
+        }
     }
 
     const handleCopy = () => {
@@ -24,15 +44,27 @@ const BlurTool = () => {
     return ( 
         <div className="max-w-lg mx-auto">
             <div className="mb-[3em]">
-                <figure>
-                    <img src="/img/img_mountain.jpg" style={{filter: `blur(${value}px)`}} alt="Contoh" className="w-full h-auto rounded-md" />
-                </figure>
+                <div className="bg-[url('/img/img_mountain.jpg')] bg-cover bg-center h-64 w-full flex justify-center items-center ">
+                    <div style={{backdropFilter: `${filterState.filter}(${value + filterState.satuan})`}} className="bg-white/30  h-46 w-64 backdrop-invert" ></div>  
+                </div>
             </div>
-            <div className="mb-5">
-                <label className="block mb-3 text-sm font-medium text-gray-900 dark:text-white">size</label>
-                <div className="flex items-center  gap-5">
-                    <input type="range" max="500" value={value} name="input-deg" id="input-size" onChange={handleBlur} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
-                    <span id="preview-size" className="font-semibold w-1/4 inline-flex text-gray-900 dark:text-white">{value} px</span>
+            <div class="md-5 flex items-center justify-between gap-6">
+                <div className="w-1/2">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                    <select id="backdropFilter" onChange={handleOption} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        {Object.keys(filterOptions).map((item, index) => (
+                            <option key={index} value={item}>
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="mb-5 w-1/2">
+                    <label className="block mb-3 text-sm font-medium text-gray-900 dark:text-white">size</label>
+                    <div className="flex items-center  gap-5">
+                        <input type="range" min="0.5"  max={filterState.maxValue} step="0.5" value={value} name="input-deg" id="input-size" onChange={(e) => setValue(e.target.value)} className=" h-2 bg-gray-200 rounded-lg w-full appearance-none cursor-pointer dark:bg-gray-700" />
+                        <span id="preview-size" className="font-semibold  text-gray-900 dark:text-white">{value}</span>
+                    </div>
                 </div>
             </div>
             <div>
@@ -45,7 +77,7 @@ const BlurTool = () => {
                         </button>
                     </div>
                         <pre className="whitespace-pre text-left overflow-x-auto">
-                            <code id="code-preview" ref={codeRef}  className="text-green-400 text-sm flex">{`blur(${value}px)`}</code>
+                            <code id="code-preview" ref={codeRef}  className="text-green-400 text-sm flex">{`backdrop-${filterState.filter}-[${value + filterState.satuan}]`}</code>
                         </pre>
                     </div>
                 </section>
